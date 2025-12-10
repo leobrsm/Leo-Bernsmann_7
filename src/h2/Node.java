b431e158f49bb5e1fb255c08cf50895e55f4f243
Leo Bernsmann <leo@MacBook-Air-von-Leo.local>
2025-12-10 17:07:18 +0100

    3

--- src/h2/Node.java
+++ src/h2/Node.java
@@ -2,4 +2,14 @@ package h2;
 
 public class Node {
 
-}
+	    
+	    public int value;   
+	    public Node next;  
+
+	    public Node(int value) {
+	        this.value = value;
+	        this.next = null;
+	    }
+	}
+
+
--- src/h2/SimpleList.java
+++ src/h2/SimpleList.java
@@ -2,4 +2,88 @@ package h2;
 
 public class SimpleList {
 
-}
+	    public Node head;
+
+	    public SimpleList() {
+	        head = new Node(Integer.MIN_VALUE); 
+	        head.next = null;                  
+	    }
+	    
+	    public Node getFirst() {
+	        if (head.next == null) {
+	            return null;
+	        }
+	        return head.next;
+	    }
+
+	    public Node getLast() {
+	        if (head.next == null) {
+	            return null;
+	        }
+
+	        Node current = head.next;
+	        while (current.next != null) {
+	            current = current.next;
+	        }
+	        return current;
+	    }
+
+	    public void append(int newValue) {
+	        Node newNode = new Node(newValue);
+
+	        if (head.next == null) {
+	            head.next = newNode;
+	        } else {
+	            Node last = getLast();
+	            last.next = newNode;
+	        }
+	    }
+
+	    public Node findFirst(int value) {
+	        Node current = head.next;
+	        while (current != null) {
+	            if (current.value == value) {
+	                return current;
+	            }
+	            current = current.next;
+	        }
+	        return null;
+	    }
+
+	    public boolean insertAfter(int preValue, int newValue) {
+	        Node current = head.next;
+
+	        while (current != null && current.value != preValue) {
+	            current = current.next;
+	        }
+
+	        if (current == null) {
+	            return false;
+	        }
+
+	        Node newNode = new Node(newValue);
+	        newNode.next = current.next;
+	        current.next = newNode;
+
+	        return true;
+	    }
+
+	    public boolean delete(int value) {
+	        Node prev = head;       
+	        Node current = head.next; 
+
+	        while (current != null && current.value != value) {
+	            prev = current;
+	            current = current.next;
+	        }
+
+	        if (current == null) {
+	            return false;
+	        }
+
+	        prev.next = current.next;
+	        return true;
+	    }
+	}
+
+
